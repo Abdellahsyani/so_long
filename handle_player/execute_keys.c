8edfdd0d398f666e22920a637a4deffa9b187974
Abdellahsyani <syaniabdos@gmail.com>
2025-02-17 13:58:44 +0100

    handle coins

--- handle_player/execute_keys.c
+++ handle_player/execute_keys.c
@@ -26,10 +26,16 @@ void	execute_key_left(t_game *game, int new_x, int new_y)
 	exit_x = game->matrix->exit_x;
 	exit_y = game->matrix->exit_y;
 	game->matrix->map[game->player_x][game->player_y] = '0';
+	if (game->matrix->map[new_x][new_y] == 'C')
+		game->matrix->coin--;
+	printf("coin after eat: %d\n", game->matrix->coin);
 	game->matrix->map[new_x][new_y] = 'P';
-	if (game->matrix->map[new_x] == game->matrix->map[exit_x] &&
-		game->matrix->map[new_y] == game->matrix->map[exit_y])
-		exit_game(game);
+	if (game->matrix->coin == 0)
+	{
+		if (game->matrix->map[new_x] == game->matrix->map[exit_x] &&
+			game->matrix->map[new_y] == game->matrix->map[exit_y])
+			exit_game(game);
+	}
 	floor_draw(game->win, game->player_x, game->player_y);
 	game->player_x = new_x;
 	game->player_y = new_y;
@@ -50,10 +56,16 @@ void	execute_key_right(t_game *game, int new_x, int new_y)
 	exit_x = game->matrix->exit_x;
 	exit_y = game->matrix->exit_y;
 	game->matrix->map[game->player_x][game->player_y] = '0';
+	if (game->matrix->map[new_x][new_y] == 'C')
+		game->matrix->coin--;
+	printf("coin after eat: %d\n", game->matrix->coin);
 	game->matrix->map[new_x][new_y] = 'P';
-	if (game->matrix->map[new_x] == game->matrix->map[exit_x] &&
-		game->matrix->map[new_y] == game->matrix->map[exit_y])
-		exit_game(game);
+	if (game->matrix->coin == 0)
+	{
+		if (game->matrix->map[new_x] == game->matrix->map[exit_x] &&
+			game->matrix->map[new_y] == game->matrix->map[exit_y])
+			exit_game(game);
+	}
 	floor_draw(game->win, game->player_x, game->player_y);
 	game->player_x = new_x;
 	game->player_y = new_y;
@@ -74,6 +86,9 @@ void	execute_key_up(t_game *game, int new_x, int new_y)
 	exit_x = game->matrix->exit_x;
 	exit_y = game->matrix->exit_y;
 	game->matrix->map[game->player_x][game->player_y] = '0';
+	if (game->matrix->map[new_x][new_y] == 'C')
+		game->matrix->coin--;
+	printf("coin after eat: %d\n", game->matrix->coin);
 	game->matrix->map[new_x][new_y] = 'P';
 	if (game->matrix->coin == 0)
 	{
@@ -101,10 +116,16 @@ void	execute_key_down(t_game *game, int new_x, int new_y)
 	exit_x = game->matrix->exit_x;
 	exit_y = game->matrix->exit_y;
 	game->matrix->map[game->player_x][game->player_y] = '0';
+	if (game->matrix->map[new_x][new_y] == 'C')
+		game->matrix->coin--;
+	printf("coin after eat: %d\n", game->matrix->coin);
 	game->matrix->map[new_x][new_y] = 'P';
-	if (game->matrix->map[new_x] == game->matrix->map[exit_x] &&
-		game->matrix->map[new_y] == game->matrix->map[exit_y])
-		exit_game(game);
+	if (game->matrix->coin == 0)
+	{
+		if (game->matrix->map[new_x] == game->matrix->map[exit_x] &&
+			game->matrix->map[new_y] == game->matrix->map[exit_y])
+			exit_game(game);
+	}
 	floor_draw(game->win, game->player_x, game->player_y);
 	game->player_x = new_x;
 	game->player_y = new_y;
